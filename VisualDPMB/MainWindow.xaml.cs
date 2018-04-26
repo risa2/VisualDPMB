5849c68ed185966c50bf20e1a141b63f615f11b1
risa2 <blazek.risa@gmail.com>
2018-04-26 16:50:42 +0200

    Map

--- VisualDPMB/MainWindow.xaml.cs
+++ VisualDPMB/MainWindow.xaml.cs
@@ -1,21 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net.Http;
-using System.Runtime.Serialization;
 using Newtonsoft.Json;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
 
 namespace VisualDPMB
 {
@@ -46,12 +38,19 @@ namespace VisualDPMB
                 var str = client.GetStringAsync("http://sotoris.cz/DataSource/CityHack2015/vehiclesBrno.aspx").Result;
                 var vehicles = JsonConvert.DeserializeObject<List<VehicleInfo>>(str);
                 platno.Children.Clear();
+
+                image.Width = platno.Width;
+                image.Height = platno.Height;
+                image.Source = new BitmapImage(new Uri("/mapa.gif", UriKind.Relative));
+                image.Stretch = Stretch.Fill;
                 foreach (var vehicle in vehicles)
                 {
-                    var e = new Ellipse();
-                    e.Width = 5;
-                    e.Height = 5;
-                    e.Fill = System.Windows.Media.Brushes.GreenYellow;
+                    var e = new Ellipse
+                    {
+                        Width = 5,
+                        Height = 5,
+                        Fill = Brushes.GreenYellow
+                    };
                     Canvas.SetLeft(e, vehicle.RelativeX(16.4, 17)*Width);
                     Canvas.SetTop(e, vehicle.RelativeY(49.1, 49.4)*Height);
                     platno.Children.Add(e);
